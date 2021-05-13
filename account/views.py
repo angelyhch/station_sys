58ce4f4dc683e2c03c931079f628e0184a90738b
angelyhch <angelyhch@163.com>
2021-05-13 14:32:26 +0800

    temp

--- account/views.py
+++ account/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 
+
 def user_login(request):
     if request.method == 'POST':
         login_form = LoginForm(request.POST)
