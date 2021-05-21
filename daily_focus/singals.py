ed78479b1fb83a4f09bb063319f6c329ec16f43d
angelyhch <angelyhch@163.com>
2021-05-21 21:38:21 +0800

    增加了logger 记录到文件功能

--- daily_focus/singals.py
+++ daily_focus/singals.py
@@ -1,17 +1,22 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
-from daily_focus.models import FocusImage, FocusAfterImage
+from daily_focus.models import Focus, FocusImage, FocusAfterImage
 from craft.utils import logger
+
+
+@receiver(pre_delete, sender=Focus)
+def Focus_delete(instance, **kwargs):
+    logger.info(f'{instance.user.username} delete id[{instance.id}]')
 
 
 @receiver(pre_delete, sender=FocusImage)
 def image_delete(instance, **kwargs):
-    logger.info('pre delete.')
+    logger.info(f'{instance.focus.user.username} delete id[{instance.id}]')
     instance.image.delete(False)
 
 
 @receiver(pre_delete, sender=FocusAfterImage)
 def after_image_delete(instance, **kwargs):
-    logger.info('pre delete.')
+    logger.info(f'{instance.focus.user.username} delete id[{instance.id}]')
     instance.image.delete(False)
 
